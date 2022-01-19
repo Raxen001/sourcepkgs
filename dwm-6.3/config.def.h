a565bd8d3e16a9f6a0a384bcc1d68b6069f49516
RaxenGamer001 <raxen001@tutamail.com>
2022-01-19 17:42:43 +0530

    before adding barpadding

--- dwm-6.3/config.def.h
+++ dwm-6.3/config.def.h
@@ -3,12 +3,14 @@
 
 /* appearance */
 static const unsigned int borderpx      = 4;        /* border pixel of windows */
-static const int gappx                  = 5;                 /* gaps between windows */
+static const int gappx                  = 8;                 /* gaps between windows */
 static const unsigned int snap          = 32;       /* snap pixel */
 static const unsigned int systraypinning= 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
 static const unsigned int systrayspacing= 2;   /* systray spacing */
 static const int systraypinningfailfirst= 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+#define ICONSIZE 26   /* icon size */
+#define ICONSPACING 8 /* space between icon and title */
 static const int showsystray            = 1;     /* 0 means no systray */
 static const int showbar                = 1;        /* 0 means no bar */
 static const int topbar                 = 0;        /* 0 means bottom bar */
@@ -26,7 +28,7 @@ static const char aqua[]                = "#689D6A";
 static const char orange[]              = "#D65D0E";
 static const char *colors[][3]          = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { white, gray, gray },
+	[SchemeNorm] = { white, gray, white },
 	[SchemeSel]  = { red,   gray, red  },
 };
 
